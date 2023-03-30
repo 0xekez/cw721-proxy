45a4f6fdd9277e320a93b8dec488f447be8c2263
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2023-03-30 22:21:39 +0200

    add schema files

---
+++ contracts/ics721-channel-proxy/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use ics721_channel_proxy::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
