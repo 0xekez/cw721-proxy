ef620cbea1d1e5dbb6695d6388b4893872508381
mr-t <mr-t@arkprotocol.io>
2023-12-31 13:36:50 +0100

    add more infos in attributes

--- packages/cw721-outgoing-proxy/src/lib.rs
+++ packages/cw721-outgoing-proxy/src/lib.rs
@@ -1,12 +1,12 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{
-    to_json_binary, DepsMut, Empty, Env, MessageInfo, Reply, Response, StdError, StdResult, SubMsg,
-    WasmMsg,
+    from_json, to_json_binary, DepsMut, Empty, Env, MessageInfo, Reply, Response, StdError,
+    StdResult, SubMsg, WasmMsg,
 };
 use cw721::Cw721ReceiveMsg;
 use cw721_base::ExecuteMsg;
 use cw721_outgoing_proxy_derive::cw721_receive_nft;
-use ics721_types::ibc_types::IbcOutgoingProxyMsg;
+use ics721_types::ibc_types::{IbcOutgoingMsg, IbcOutgoingProxyMsg};
 
 pub(crate) const DO_NOTHING_REPLY_ID: u64 = 1;
 
@@ -23,10 +23,17 @@ pub fn execute_receive_nft(
     ics721: String,
 ) -> StdResult<Response> {
     let Cw721ReceiveMsg {
-        sender,
+        sender: nft_owner,
         token_id,
         msg,
     } = msg;
+    let IbcOutgoingMsg {
+        channel_id,
+        memo,
+        receiver,
+        ..
+    } = from_json(&msg)?;
+
     let msg = IbcOutgoingProxyMsg {
         collection: info.sender.to_string(),
         msg,
@@ -43,8 +50,8 @@ pub fn execute_receive_nft(
     let ics721_msg = WasmMsg::Execute {
         contract_addr: ics721.to_string(), // ics721
         msg: to_json_binary(&ProxyExecuteMsg::ReceiveNft(Cw721ReceiveMsg {
-            sender,
-            token_id,
+            sender: nft_owner.clone(),
+            token_id: token_id.clone(),
             msg: to_json_binary(&msg)?,
         }))?,
         funds: vec![],
@@ -52,7 +59,13 @@ pub fn execute_receive_nft(
     let ics721_msg = SubMsg::reply_on_success(ics721_msg, DO_NOTHING_REPLY_ID);
     Ok(Response::default()
         .add_submessage(transfer_msg)
-        .add_submessage(ics721_msg))
+        .add_submessage(ics721_msg)
+        .add_attribute("nft_owner", nft_owner)
+        .add_attribute("collection", info.sender.to_string())
+        .add_attribute("token_id", token_id)
+        .add_attribute("channel_id", channel_id)
+        .add_attribute("memo", memo.unwrap_or("empty".to_string()))
+        .add_attribute("receiver", receiver))
 }
 
 pub fn reply(_deps: DepsMut, _env: Env, reply: Reply) -> StdResult<Response> {
