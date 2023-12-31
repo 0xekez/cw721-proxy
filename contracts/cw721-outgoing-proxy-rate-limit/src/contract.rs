80e52ce0466fff3ca0f5dac16245ce0d9eb41703
mr-t <mr-t@arkprotocol.io>
2023-12-31 13:02:58 +0100

    move main logic to package for re-use

--- contracts/cw721-outgoing-proxy-rate-limit/src/contract.rs
+++ contracts/cw721-outgoing-proxy-rate-limit/src/contract.rs
@@ -1,14 +1,14 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Reply, Response, StdError,
-    StdResult, SubMsg, WasmMsg,
+    to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, StdResult,
 };
 use cw2::set_contract_version;
 use cw721::Cw721ReceiveMsg;
-use cw721_base::msg::ExecuteMsg as Cw721ExecuteMsg;
-use ics721_types::ibc_types::IbcOutgoingProxyMsg;
 
+use cw721_outgoing_proxy::{
+    execute_receive_nft as cw721_execute_receive_nft, reply as cw721_reply,
+};
 use cw_rate_limiter::{Rate, RateLimitError};
 
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
@@ -16,8 +16,6 @@ use crate::state::{ORIGIN, RATE_LIMIT};
 
 const CONTRACT_NAME: &str = "crates.io:cw721-outgoing-proxy-rate-limit";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-
-pub(crate) const DO_NOTHING_REPLY_ID: u64 = 1;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -64,48 +62,16 @@ pub fn execute_receive_nft(
     msg: Cw721ReceiveMsg,
 ) -> Result<Response, RateLimitError> {
     RATE_LIMIT.limit(deps.storage, &env.block, info.sender.as_str())?;
-    // NFT send to this proxy contract, so we need to:
-    // - transfer NFT to the origin contract
-    // - call receive_nft on the origin contract
-    let Cw721ReceiveMsg {
-        sender,
-        token_id,
+    Ok(cw721_execute_receive_nft(
+        info,
         msg,
-    } = msg;
-    let msg = IbcOutgoingProxyMsg {
-        collection: info.sender.to_string(),
-        msg,
-    };
-    let transfer_msg = WasmMsg::Execute {
-        contract_addr: info.sender.to_string(), // collection contract
-        msg: to_json_binary(&Cw721ExecuteMsg::<Empty, Empty>::TransferNft {
-            recipient: ORIGIN.load(deps.storage)?.into_string(), // ics721
-            token_id: token_id.clone(),
-        })?,
-        funds: vec![],
-    };
-    let transfer_msg = SubMsg::reply_on_success(transfer_msg, DO_NOTHING_REPLY_ID);
-    let ics721_msg = WasmMsg::Execute {
-        contract_addr: ORIGIN.load(deps.storage)?.into_string(), // ics721
-        msg: to_json_binary(&ExecuteMsg::ReceiveNft(Cw721ReceiveMsg {
-            sender,
-            token_id,
-            msg: to_json_binary(&msg)?,
-        }))?,
-        funds: vec![],
-    };
-    let ics721_msg = SubMsg::reply_on_success(ics721_msg, DO_NOTHING_REPLY_ID);
-    Ok(Response::default()
-        .add_submessage(transfer_msg)
-        .add_submessage(ics721_msg))
+        ORIGIN.load(deps.storage)?.to_string(),
+    )?)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn reply(_deps: DepsMut, _env: Env, reply: Reply) -> StdResult<Response> {
-    match reply.id {
-        DO_NOTHING_REPLY_ID => Ok(Response::default()),
-        _ => Err(StdError::generic_err("reply id not recognized")),
-    }
+    cw721_reply(_deps, _env, reply)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
--- packages/cw721-outgoing-proxy/src/lib.rs
+++ packages/cw721-outgoing-proxy/src/lib.rs
@@ -1,6 +1,63 @@
 use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{
+    to_json_binary, DepsMut, Empty, Env, MessageInfo, Reply, Response, StdError, StdResult, SubMsg,
+    WasmMsg,
+};
+use cw721::Cw721ReceiveMsg;
+use cw721_base::ExecuteMsg;
 use cw721_outgoing_proxy_derive::cw721_receive_nft;
+use ics721_types::ibc_types::IbcOutgoingProxyMsg;
+
+pub(crate) const DO_NOTHING_REPLY_ID: u64 = 1;
 
 #[cw721_receive_nft]
 #[cw_serde]
 pub enum ProxyExecuteMsg {}
+
+/// NFT send to this proxy contract, so we need to:
+/// - transfer NFT to the ics721 contract (origin)
+/// - call receive_nft on the ics721 contract
+pub fn execute_receive_nft(
+    info: MessageInfo,
+    msg: Cw721ReceiveMsg,
+    ics721: String,
+) -> StdResult<Response> {
+    let Cw721ReceiveMsg {
+        sender,
+        token_id,
+        msg,
+    } = msg;
+    let msg = IbcOutgoingProxyMsg {
+        collection: info.sender.to_string(),
+        msg,
+    };
+    let transfer_msg = WasmMsg::Execute {
+        contract_addr: info.sender.to_string(), // collection contract
+        msg: to_json_binary(&ExecuteMsg::<Empty, Empty>::TransferNft {
+            recipient: ics721.to_string(), // ics721
+            token_id: token_id.clone(),
+        })?,
+        funds: vec![],
+    };
+    let transfer_msg = SubMsg::reply_on_success(transfer_msg, DO_NOTHING_REPLY_ID);
+    let ics721_msg = WasmMsg::Execute {
+        contract_addr: ics721.to_string(), // ics721
+        msg: to_json_binary(&ProxyExecuteMsg::ReceiveNft(Cw721ReceiveMsg {
+            sender,
+            token_id,
+            msg: to_json_binary(&msg)?,
+        }))?,
+        funds: vec![],
+    };
+    let ics721_msg = SubMsg::reply_on_success(ics721_msg, DO_NOTHING_REPLY_ID);
+    Ok(Response::default()
+        .add_submessage(transfer_msg)
+        .add_submessage(ics721_msg))
+}
+
+pub fn reply(_deps: DepsMut, _env: Env, reply: Reply) -> StdResult<Response> {
+    match reply.id {
+        DO_NOTHING_REPLY_ID => Ok(Response::default()),
+        _ => Err(StdError::generic_err("reply id not recognized")),
+    }
+}
