4d6e3b0e168305044db00176494a28b318ae8dfe
mr-t <mr-t@arkprotocol.io>
2024-01-02 15:04:55 +0100

    cleanup

--- contracts/cw-ics721-outgoing-proxy-rate-limit/src/bin/schema.rs
+++ contracts/cw-ics721-outgoing-proxy-rate-limit/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cw721_outgoing_proxy_rate_limit::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_ics721_outgoing_proxy_rate_limit::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
